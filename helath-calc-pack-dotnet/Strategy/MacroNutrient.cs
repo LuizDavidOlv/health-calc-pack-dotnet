c5a70536f819ee711d80681d9c8dc380a0430614
Luiz David <luizdavid.93@hotmail.com>
2022-11-11 14:55:53 -0300

    Initial Commit

---
+++ helath-calc-pack-dotnet/Strategy/MacroNutrient.cs
@@ -0,0 +1,13 @@
+﻿//using helath_calc_pack_dotnet.Interfaces;
+//using helath_calc_pack_dotnet.Models;
+
+//namespace helath_calc_pack_dotnet.Strategy
+//{
+//    public class MacroNutrient : IMacroNutrient
+//    {
+//        public MacroNutrientsModel Calc(
+//            SexEnum Sex,
+//            double Heigth,
+//            double Weight)
+//    }
+//}
---
+++ helath-calc-pack-dotnet/Strategy/MaintenanceStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helath_calc_pack_dotnet.Strategy
+{
+    public class MaintenanceStrategy
+    {
+    }
+}
